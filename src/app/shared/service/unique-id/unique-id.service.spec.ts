8a8b1782fab9c42b592c621c765bb8817dd86e6e
keomaklein <keoma.klein1@gmail.com>
2021-08-17 17:34:41 -0300

    Refatorado testes e adicionado teste de exception

--- src/app/shared/service/unique-id/unique-id.service.spec.ts
+++ src/app/shared/service/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,43 @@
 import { UniqueIdService } from './unique-id.service';
-describe('UniqueIdService', () =>{
-  it('#generateUniqueIdWithPrefix should generate id when with prefix', ()=>{
-    const service = new UniqueIdService();
-    const id = service.generateUniqueIdWithPrefix('app');
 
-    expect(id).toContain('app-');
+describe('UniqueIdService', () => {
+
+  let service: UniqueIdService = null;
+
+  beforeEach(() => {
+    service = new UniqueIdService();
   })
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let index = 0; index < 50; index++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGenerateUniqueIds.name}
+    should return the number of generatedIds when called`, () => {
+    const ids = [];
+    for (let index = 0; index < 4; index++) {
+      ids.push(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.length).toBe(4);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should throw when called with empty`, () => {
+      const emptyValues = [null, undefined, ''];
+
+      emptyValues.forEach(ev => {
+        expect(()=>service.generateUniqueIdWithPrefix(ev)).toThrow();
+      })
+  });
 })
